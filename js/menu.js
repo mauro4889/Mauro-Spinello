b45bbb1621b81cecdf1b2b12a60846b95c5984e3
Mauro Spinello <mauro4889@gmail.com>
2022-10-09 14:03:36 -0300

    Primer commit

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+document.querySelector('.btn_menu').addEventListener('click', () => {
+    event.stopPropagation()
+    animateBars()
+});
+
+let line_1 = document.querySelector('.line-1');
+let line_2 = document.querySelector('.line-2');
+let line_3 = document.querySelector('.line-3');
+let nav = document.getElementById('nav')
+let nav_menu = document.querySelector('.nav_menu');
+
+function animateBars() {
+    line_1.classList.toggle('active_line-1');
+    line_2.classList.toggle('active_line-2');
+    line_3.classList.toggle('active_line-3');
+    nav.classList.toggle('responsive')
+}
+
+window.onclick = function(e) {
+    animateBars()
+}
